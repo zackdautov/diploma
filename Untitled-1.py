f9fe9a36dc9e9b2c761186272d2a47cfede3f38d
unknown <dautovza@gmail.com>
2025-03-12 17:22:22 +0300

    cached

--- Untitled-1.py
+++
@@ -1,4 +0,0 @@
-a = " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
-if "ipsum" in a:
-    a = a.replace("ipsum", "", 1) + " ipsum"
-print(a)
